27acd1be070fd04887dfda579f66a0eff0444b19
JulienProulx <2077973@cstj.qc.ca>
2023-02-20 14:13:02 -0500

    git init

---
+++ src/App.vue
@@ -0,0 +1,16 @@
+<script setup>
+import {RouterView } from 'vue-router'
+import HeaderApp from "@/components/commun/HeaderApp/HeaderApp.vue";
+import FooterApp from "@/components/commun/FooterApp/FooterApp.vue";
+</script>
+
+<template>
+  <h1>Structure de l'app</h1>
+  <HeaderApp />
+  <FooterApp />
+  <RouterView />
+</template>
+
+<style scoped>
+
+</style>
---
+++ src/components/commun/FooterApp/FooterApp.vue
@@ -0,0 +1,13 @@
+<script setup></script>
+
+<template>
+    <main>
+        <header>
+            <h3>Footer</h3>
+        </header>
+    </main>
+</template>
+
+<style lang ="scss">
+
+</style>
