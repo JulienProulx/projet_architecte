90eff6c5372bb67c7ff47e8f3e8f5cdf3b3aa916
JulienProulx <2077973@cstj.qc.ca>
2023-02-27 13:18:30 -0500

    fix-reorganisation-app

--- src/App.vue
+++ src/App.vue
@@ -1,14 +1,14 @@
 <script setup>
 import {RouterView } from 'vue-router'
-import HeaderIndexApp from "@/components/commun/HeaderIndexApp/HeaderIndexApp.vue";
 import FooterApp from "@/components/commun/FooterApp/FooterApp.vue";
 </script>
 
 <template>
-  <h1>Structure de l'app</h1>
-  <HeaderIndexApp />
-  <FooterApp />
+<div class="structure">
   <RouterView />
+  <FooterApp />
+</div>
+
 </template>
 
 <style scoped>
