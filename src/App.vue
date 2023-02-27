87da62e3e02a51e9f31f2426b4cdb894acb168fb
JulienProulx <2077973@cstj.qc.ca>
2023-02-27 10:10:52 -0500

    ajout-structure-scss

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,12 @@
 <script setup>
 import {RouterView } from 'vue-router'
-import HeaderApp from "@/components/commun/HeaderApp/HeaderApp.vue";
+import HeaderIndexApp from "@/components/commun/HeaderIndexApp/HeaderIndexApp.vue";
 import FooterApp from "@/components/commun/FooterApp/FooterApp.vue";
 </script>
 
 <template>
   <h1>Structure de l'app</h1>
-  <HeaderApp />
+  <HeaderIndexApp />
   <FooterApp />
   <RouterView />
 </template>
