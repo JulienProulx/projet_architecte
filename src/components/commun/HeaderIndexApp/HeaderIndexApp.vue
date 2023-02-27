fa00854d4ae83db36061d827206a90ea52161d9e
JulienProulx <2077973@cstj.qc.ca>
2023-02-27 10:09:30 -0500

    feature-ajout-svg

---
+++ src/components/commun/HeaderIndexApp/HeaderIndexApp.vue
@@ -0,0 +1,13 @@
+<script setup></script>
+
+<template>
+    <main>
+        <header>
+            <h3>Header</h3>
+        </header>
+    </main>
+</template>
+
+<style lang = "scss">
+    
+</style>
